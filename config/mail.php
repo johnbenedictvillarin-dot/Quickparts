a38642122fbd68cd69b6f4bfebdfc70f161e338e
Quickparts <johnbenedictvillarin@gmail.com>
2026-05-18 13:31:42 +0800

    Set SMTP timeout to 5s to avoid 60s hang

--- config/mail.php
+++ config/mail.php
@@ -11,7 +11,7 @@ return [
             'encryption' => env('MAIL_ENCRYPTION', 'tls'),
             'username' => env('MAIL_USERNAME'),
             'password' => env('MAIL_PASSWORD'),
-            'timeout' => null,
+            'timeout' => 5,
             'auth_mode' => null,
         ],
     ],
